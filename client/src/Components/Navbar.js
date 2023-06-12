98f25c9d00cf3eac45aa93e164c53b09a1d2fdad
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-12 16:26:25 +0530

    add cart icon

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,27 +1,32 @@
 import React from 'react'
 // import Hero from './hero/hero';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './navbar.css'
+import { BsCart } from 'react-icons/bs'
 
 
-const Navbar =()=> {
+const Navbar = () => {
     return (
         <div>
             <nav className="navbar">
-                <h3 >YUMMY PIZZA</h3>
-                
+                <div className="container">
+                    <h3 className="logo">YUMMY PIZZA</h3>
+
                     <ul className="nav-links">
                         <Link to='/'><li>Home</li></Link>
-                        <Link to='/'><li>Menu</li></Link>
-                        {/* <Link to='/'><li>Cart</li></Link> */}
+                        <Link to='/menu'><li>Menu</li></Link>
+                        <Link to='/login'><li>Login</li></Link>
+                        <Link to='/cart'><li>
+                            <button className='cartIcon'>
+                                <BsCart />
+                            </button>
+                        </li></Link>
                     </ul>
-                    {/* <div className='d-flex'>
-                        <a className="btn bg-white text-success mx-1" href="#">Login</a>
-                        <a className="btn bg-white text-success mx-1" href="#">Cart</a>
-                    </div> */}
+
+                </div>
             </nav>
         </div>
-       
+
     )
 }
 export default Navbar
