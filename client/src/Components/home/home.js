c19dd95292aef3f6e73418ea2592653a274423c1
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-12 21:09:48 +0530

    create section two in home page

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -4,7 +4,8 @@ import manEating from '../../assessts/pizza1.png'
 
 const Home = () => {
   return (
-    <section id="home" className="container">
+    <section>
+    <section style={{height:'90vh'}} id="home" className="container">
       <div className="wrapper">
         <div className="left">
           <h2 className="tittle">Do you crave delicious food</h2>
@@ -21,6 +22,33 @@ const Home = () => {
           <img src={manEating} alt="" className="manEating"></img>
         </div>
       </div>
+    </section>
+    <section >
+      <div className='container'>
+      <div className="wrapper">
+        <div className="delivery">
+          <div className='titles'>
+            <span className='deliverysubtitle'>Delivery</span>
+            <h2 className='deliveriTitle'>Always on time for you</h2>
+          </div>
+          <div className='deliveryInfo'>
+            <div className='deliveryInfo'>
+              <img src={illustration1} alt='' className='firstImg'/>
+              <h3>Our delivery guy is always on time</h3>
+            </div>
+            <div className='deliveryInfo'>
+              <img src={illustration2} alt='' className='secondImg'/>
+              <h3>He works very hard</h3>
+            </div>
+            <div className='deliveryInfo'>
+              <img src={illustration3} alt='' className='thirdImg'/>
+              <h3>He is friendly and social</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
     </section>
   )
 
