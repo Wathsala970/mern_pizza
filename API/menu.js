a622b6c6776d1ddc0c7320d5f86f12d819df2672
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-14 08:34:13 +0530

    create menu

--- API/menu.js
+++ API/menu.js
@@ -5,15 +5,27 @@ const router = express.Router();
 
 const menu = [
     {
-        name : "wathsala",
-        age : 24
-    }
+        name : "PEPPER BARBECUE & ONION",
+        varients : ["Small","Medium","Large"],
+        price : [{"Small": 999,
+        "Medium":1849,
+        "Large": 3399} ],
+        catogary : "none-veg",
+        image : "https://www.dominos.co.in/files/items/Pepper_Barbeque_&_Onion.jpg",
+        description: "Pepper Barbecue Chicken I Onion"
+    },
+    
 ]
 
 router.get('/', (req,res)=>{
-    console.log(menu);
+    // console.log(menu);
     res.send(menu)
 });
 
+router.post('/', (req,res)=>{
+    const pizza = req.body;
+    menu.push(pizza);
+    res.send('Data added to the database');
+});
 
 module.exports = router;
