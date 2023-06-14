1083b6257deacd6de4d83e13df41b437aa4c8614
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-14 10:10:11 +0530

    add image using json file

--- client/src/Components/Menu/pizza.js
+++ client/src/Components/Menu/pizza.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function Pizza({pizza}){
     return(
         <div>
-            <h1>{pizza.name}</h1>
-            {/* <img src={pizza.image} className="img-fluid"/> */}
+            <h1 className="title">{pizza.name}</h1>
+            <img src={pizza.image} className="img-fluid"/>
         </div>
     )
 }
