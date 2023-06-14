0940bd86a55a3ba9a9be089a8073846cce5660c6
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-14 11:02:45 +0530

    create varients for select

--- client/src/Components/Menu/pizza.js
+++ client/src/Components/Menu/pizza.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 
-export default function Pizza({pizza}){
-    return(
-        <div>
+export default function Pizza({ pizza }) {
+    return (
+        <div className="container">
             <h1 className="title">{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid"/>
+            <img src={pizza.image} className="img-fluid" />
+            <div className="flex-container">
+                <div className="w-100">
+                    <p>Varients</p>
+                    <select>
+                        {pizza.varients.map(varient=>{
+                            return<option value={varient}>{varient}</option>
+                        })}
+                    </select>
+                </div>
+                <div className="w-100">
+                    <p>Quentity</p>
+                </div>
+            </div>
         </div>
     )
 }
