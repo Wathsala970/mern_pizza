f0f98df919002ce77a50d0f514e5ce0a7d6dd674
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-15 22:38:09 +0530

    connect pizza routes

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const Pizza = require('./server/models/pizzaModel')
 const app = express();
 app.use(express.json());
 
+const pizzasRoute = require('./server/routes/pizzaRaoutes')
+
+app.use('/api/pizzas/', pizzasRoute)
+
 app.get("/", (req,res)=>{
     res.send("server is working " + port);
 });
 
-app.get('/getpizzas', async (req,res)=>{
-    try {
-        const pizzas = await Pizza.find({});
-        res.send(pizzas)
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred");
-    }
-});
-
+// app.get('/getpizzas', async (req,res)=>{
+//     try {
+//         const pizzas = await Pizza.find({});
+//         res.send(pizzas)
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("An error occurred");
+//     }
+// });
 
 app.use('/menu', menuRoutes)
 const port = process.env.PORT || 8000;
