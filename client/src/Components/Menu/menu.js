651a921271506b050da6baf179c4c1ffa7c0e709
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-15 22:41:02 +0530

    change some styles

--- client/src/Components/Menu/menu.js
+++ client/src/Components/Menu/menu.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import pizzas from '../Menu/pizzasdata'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPizzas } from '../actions/pizzaActions'
 import Pizza from '../Menu/pizza';
 import './menu.css';
 
-export default function Menu(){
-  return(
-      <div>
-          <div className='row'>
-            {pizzas.map(pizza=>{
-              return <div className='col-md-4'>
-                <div>
-                  <Pizza pizza={pizza}/>
-                </div>
-              </div>
-            })}
+export default function Menu() {
+  const dispatch = useDispatch()
+
+  const pizzasstate = useSelector(state => state.getAllPizzasReducer)
+  const { pizzas, error, loading } = pizzasstate
+  useEffect(() => {
+    dispatch(getAllPizzas())
+  }, [])
+
+
+  return (
+    <div>
+      <div className='row justify-content-center'>
+
+        {loading ? (
+        <h1 className='title2 m-4'>Loading....</h1>
+        ) : error ? (
+        <h1 className='title2 m-3 '>Something went wrong</h1>
+        ) : (
+        pizzas.map(pizza => {
+          return (
+          <div className='col-md-3 m-3' key={pizza._id}>
+            <div >
+              <Pizza pizza={pizza} />
+            </div>
           </div>
+          );
+          })
+        )}
       </div>
+    </div>
   )
 }
