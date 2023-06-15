88d07cf5f16319fa78a6cc3f431504c08923f4d6
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-15 19:18:35 +0530

    change imported files location

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('./server/db')
+const menuRoutes = require('./API/Menu/menu')
+const Pizza = require('./server/models/pizzaModel')
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("server is working " + port);
+});
+
+app.get('/getpizzas', async (req,res)=>{
+    try {
+        const pizzas = await Pizza.find({});
+        res.send(pizzas)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+
+app.use('/menu', menuRoutes)
+const port = process.env.PORT || 8000;
+app.listen(port, ()=> 'server running on port')
