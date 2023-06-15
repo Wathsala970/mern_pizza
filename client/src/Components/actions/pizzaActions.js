57ec3e45f85b0f9d1435f81a84e4a7e8b07f2f26
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-15 22:43:07 +0530

    create pizza actions

--- client/src/Components/actions/pizzaActions.js
+++ client/src/Components/actions/pizzaActions.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-export const getAllPizzas = ()=>dispatch=>{
+export const getAllPizzas = () => async dispatch=>{
     dispatch({type: 'GET_PIZZAS_REQUEST'})
 
     try {
-        const response = axios.get('/api/pizzas/getpizzas')
+        const response = await axios.get('api/pizzas/getallpizzas')
         console.log(response);
-        dispatch({type: 'GET_PIZZAS_SUCCESS' , payload : response.data})
+        dispatch({type: 'GET_PIZZAS_SUCCES', payload : response.data})
     } catch (error) {
-        dispatch({type: 'GET_PIZZAS_FAILED',  payload : error})
+        dispatch({type: 'GET_PIZZAS_FAILED', payload : error})
         
     }
 }
