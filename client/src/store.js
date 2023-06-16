c8181fb32b1c2b5fe2972a1b0f64457a59e6e9f2
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-16 17:37:17 +0530

    add cart reducer

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,20 @@ import {createStore , applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {getAllPizzasReducer} from '../src/Components/reducers/pizzaReducers';
+import { CartReducer } from './Components/reducers/cartReducers';
 
 const finalReducer = combineReducers({
-    getAllPizzasReducer : getAllPizzasReducer
+    getAllPizzasReducer : getAllPizzasReducer,
+    CartReducer : CartReducer
 })
 
-const initialState = {}
+const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    CartReducer :{
+        cartItems : cartItems
+    }
+}
 const composeEnhancers = composeWithDevTools({})
 const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
 
