cc200067381bcb802ac77aae0fedc4b62f7668c2
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-16 19:50:25 +0530

    change cart name

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route , Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route , Routes , Link , Switch} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/home/home';
 import Menu from './Components/Menu/menu';
 import Login from './Components/Login/login';
-import Cart from './Components/Cart/cart';
+import CartScreen from './Components/Cart/cart';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/menu' element={<Menu/>} />
           <Route path='/login' element={<Login/>} />
-          <Route path='/cart' element={<Cart/>} />
+          <Route path='/cart' element={<CartScreen/>} />
         </Routes>
       </Router>
     </>
