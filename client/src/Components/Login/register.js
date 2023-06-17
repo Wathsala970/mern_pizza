c31519816d579a8cce0815180f508a362d37bab9
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-17 12:40:21 +0530

    create register page

---
+++ client/src/Components/Login/register.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Registration(){
+    return(
+        <div>
+            <div className='row justify-content-center'>
+                <div className='col-md-5'>
+                    <div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+  
