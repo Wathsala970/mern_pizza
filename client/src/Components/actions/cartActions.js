0549eef33f660a85f039915ec3eb1074998c7eee
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-17 12:46:58 +0530

    create cart actions

--- client/src/Components/actions/cartActions.js
+++ client/src/Components/actions/cartActions.js
@@ -15,3 +15,9 @@ export const addToCart=(pizza, quantity, varient)=>(dispatch , getState )=>{
     const cartItems = getState().CartReducer.cartItems
     localStorage.setItem('cartItems' , JSON.stringify(cartItems))
 }
+
+export const deleteFromCart=(pizza)=>(dispatch, getState)=>{
+    dispatch({type: 'DELETE_FROM_CART', payload : pizza})
+    const cartItems = getState().CartReducer.cartItems
+    localStorage.setItem('cartItems' , JSON.stringify(cartItems))
+}
