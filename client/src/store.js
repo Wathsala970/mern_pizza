b886c246c41e1ddfc10ae0257ea19d0833c74473
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-17 15:40:11 +0530

    add register user reducer

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,12 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {getAllPizzasReducer} from '../src/Components/reducers/pizzaReducers';
 import { CartReducer } from './Components/reducers/cartReducers';
+import {registerUserReducer} from '../src/Components/reducers/userReducer'
 
 const finalReducer = combineReducers({
     getAllPizzasReducer : getAllPizzasReducer,
-    CartReducer : CartReducer
+    CartReducer : CartReducer,
+    registerUserReducer : registerUserReducer
 })
 
 const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
