13d76c80d69401eba52d46162ff9f26cfac7c949
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-17 12:44:01 +0530

    create input containers

--- client/src/Components/Login/register.js
+++ client/src/Components/Login/register.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-export default function Registration(){
-    return(
+export default function Registration() {
+    return (
         <div>
             <div className='row justify-content-center'>
                 <div className='col-md-5'>
                     <div>
-                       
+                        <input type='text' placeholder='name' className='form-control' />
+                        <input type='text' placeholder='email' className='form-control' />
+                        <input type='text' placeholder='password' className='form-control' />
+                        <input type='text' placeholder='confirm password' className='form-control' />
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-  
