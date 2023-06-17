1aee3ca1ad9b84d2007a91b9cb4da399be815178
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-17 12:43:32 +0530

    create cart items

--- client/src/Components/Cart/cart.js
+++ client/src/Components/Cart/cart.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { addToCart } from '../actions/cartActions';
+import { deleteFromCart } from '../actions/cartActions';
 
 export default function CartScreen() {
   const cartstate = useSelector(state => state.CartReducer)
   const cartItems = cartstate.cartItems
+  var subtotal = cartItems.reduce((x, item) => x + item.price, 0)
+  const dispatch = useDispatch()
   return (
     <div>
-      <div className='row'>
-        <div className='col-md-6'>
-          <h3 style={{ fontSize: '40px' }} >My Cart</h3>
+      <div className='row '>
+        <div className='col m-4 '>
+          <h3>My Cart</h3>
           {cartItems.map(item => {
 
             return <div className='flex-container'>
-
-              <div className='text-left m-1 w-100'>
-                <h5>{item.name}[{item.varient}]</h5>
-                <h5>Price : {item.quantity} * {item.prices[0][item.varient]} = {item.price}</h5>
-                <h5 style={{ display: 'inline' }}>Quantity : </h5>
+              <div className='text-left m-4 w-100'>
+                <h5>{item.name} [{item.varient}]</h5>
+                <h5>Price : {item.quantity} * {item.prices[0][item.varient]}.00 = {item.price}.00</h5>
+                {/* <h5 style={{ display: 'inline' }}>Quantity : </h5>
                 <i className="fa fa-plus" aria-hidden="true"></i>
-                <b className='quantity'>{item.quantity}</b>
-                <i className="fa fa-minus" aria-hidden="true"></i>
+                <h5 style={{ display: 'inline' }}>{item.quantity}</h5>
+                <i className="fa fa-minus" aria-hidden="true"></i> */}
                 <hr />
               </div>
 
-              <div className='m-1 w-100'>
-                <img src={item.image} style={{ height: '100px', width: '100px' }}></img>
+              <div className='m-1  '>
+                <img src={item.image} style={{ height: '100px', width: '100px' }} />
               </div>
-              <div className='m-1 w-100'>
-                <i className="fa fa-trash" aria-hidden="true"></i>
+
+              <div className='m-1 w-50'>
+              <i className="fa fa-trash" aria-hidden="true" onClick={()=>{dispatch(deleteFromCart(item))}}></i>
               </div>
 
             </div>
           })}
 
+        </div>
+        <div className=' col m-5 ' >
+          <h3>Sub Total : {subtotal}.00 /=</h3>
+          <button className='btn'>CHECK OUT</button>
         </div>
       </div>
     </div>
