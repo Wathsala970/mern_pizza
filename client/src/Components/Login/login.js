51b57e0e31fcb8607e44fda169703241cc771e5a
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 09:25:21 +0530

    create click here to register

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import {loginUser } from '../actions/userAction';
+
 
 export default function Login(){
 
-    const [email, setemail] = useState('')
-    const [password, setpassword] = useState('')
+    const [email, setemail] = useState('');
+    const [password, setpassword] = useState('');
+    const dispatch = useDispatch()
 
     
 
+    function login(){
+        const user = {email, password}
+        dispatch(loginUser(user))
+    }
+    useEffect(()=>{
+        if (localStorage.getItem('currentUser'))
+        {
+            window.location.href='/'
+        }
+    },[])
+
+
     return(
-        <div>
-            <div className='row justify-content-center mt-5'>
-                <div className='col-md-5 mt-5 text-left'>
+        <div class="container">
+            <div className='row justify-content-center mt-5 '>
+                <div className='col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded'>
                     <h3 className='text-center'>Login</h3>
                     <div className='input'>
 
@@ -32,7 +48,9 @@ export default function Login(){
                         onChange={(e)=>{setpassword(e.target.value)}}
                         />
 
-                        <button className='btn'>Login</button>
+                        
+                        <button onClick={login} className='btn'>Login</button>
+                        <h6 className='text'><a style={{color: 'black'}} href='/register'>Click Here To Register</a></h6>
                     </div>
                 </div>
             </div>
