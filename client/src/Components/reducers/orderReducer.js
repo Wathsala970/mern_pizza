007eda262af682d9ca805a976e441919274c2184
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 15:43:59 +0530

    create get user order reducer

--- client/src/Components/reducers/orderReducer.js
+++ client/src/Components/reducers/orderReducer.js
@@ -16,3 +16,25 @@ export const placeOrderReducer = (state={}, action)=>{
         default : return state
     }
 }
+
+
+
+export const getUserOrdersReducer= (state={orders : []}, action) =>{
+
+    switch (action.type)
+    {
+        case 'GET_USER_ORDERS_REQUEST' : return{
+            loading: true,
+            ...state
+        }
+        case 'GET_USER_ORDERS_SUCCES' :return{
+            loading : false,
+            orders : action.payload
+        }
+        case 'GET_USER_ORDERS_FAILED' : return{
+            error : action.payload,
+            loading : false
+        }
+        default : return state
+    }
+}
