b9e2fdf5d912dffde99ea494fadffb575b54a649
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 09:29:27 +0530

    set loginuserreducer

--- client/src/store.js
+++ client/src/store.js
@@ -4,19 +4,24 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {getAllPizzasReducer} from '../src/Components/reducers/pizzaReducers';
 import { CartReducer } from './Components/reducers/cartReducers';
-import {registerUserReducer} from '../src/Components/reducers/userReducer'
+import {loginUserReducer, registerUserReducer} from '../src/Components/reducers/userReducer'
 
 const finalReducer = combineReducers({
     getAllPizzasReducer : getAllPizzasReducer,
     CartReducer : CartReducer,
-    registerUserReducer : registerUserReducer
+    registerUserReducer : registerUserReducer,
+    loginUserReducer : loginUserReducer
 })
 
 const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const currentUser = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : null
 
 const initialState = {
     CartReducer :{
         cartItems : cartItems
+    },
+    loginUserReducer:{
+        currentUser : currentUser
     }
 }
 const composeEnhancers = composeWithDevTools({})
