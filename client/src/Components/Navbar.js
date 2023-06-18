dc5e03cd703c88c6316ad20347bab48f6280815f
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 09:28:02 +0530

    create current user profile

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,13 +5,17 @@ import './navbar.css'
 import { BsCart } from 'react-icons/bs'
 import { FaBars } from 'react-icons/fa'
 import { ImCross } from 'react-icons/im'
-import {useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {logoutUser} from '../Components/actions/userAction'
 
 
-export default function Navbar(){
+export default function Navbar() {
 
     const cartstate = useSelector(state => state.CartReducer)
+    const userState = useSelector(state => state.loginUserReducer)
     const [Mobile, setMobile] = useState(false)
+    const { currentUser } = userState
+    const dispatch = useDispatch()
 
     return (
         <div>
@@ -21,13 +25,23 @@ export default function Navbar(){
 
                 <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onclick={() => setMobile(false)}>
                     <Link to='/'><li>Home</li></Link>
-                    <Link to='/menu'><li>Menu</li></Link>
-                    <Link to='/login'><li>Login</li></Link>
-                    <Link to='/cart'><li>
-                        <button className='cartIcon'>
-                            <BsCart/> {cartstate.cartItems.length}
-                        </button>
-                    </li></Link>
+
+
+                    {currentUser ? (
+                        <>
+                            <a className='nav-link m-2' href='/menu'>Menu</a>
+                            <a className='nav-link m-2' href='/login'>My Orders</a>
+                            <a className='nav-link m-2' href='/cart'><BsCart /> {cartstate.cartItems.length}</a>
+                            <li className='nav-link m-2'>{currentUser.name}</li>
+                            <a className='nav-link m-2' href='/' onClick={()=>{dispatch(logoutUser())}}><li>Logout</li></a>
+                        </>
+                    ) : (
+                        <li className='nav-link'>
+                            <a className='nav-link' href='/login'>Login</a>
+                        </li>)}
+
+
+
                 </ul>
                 <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
                     {Mobile ? <ImCross /> : <FaBars />}
