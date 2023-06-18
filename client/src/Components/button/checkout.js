fe78ea42bd7c242e2d56ff7df3a306baf855ce7b
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 15:42:57 +0530

    set loading, error, success

--- client/src/Components/button/checkout.js
+++ client/src/Components/button/checkout.js
@@ -1,10 +1,15 @@
 import React from "react";
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 import { placeOrder } from "../actions/orderActions";
+import Error from '../error';
+import Success from '../success';
+import Loading from '../loading';
 
 export default function Checkout({subtotal}){
 
+    const orderstate = useSelector((state)=> state.placeOrderReducer)
+    const {loading, error, success} = orderstate
     const dispatch = useDispatch()
     function tokenHandler(token)
     {
@@ -14,6 +19,10 @@ export default function Checkout({subtotal}){
 
     return(
         <div>
+            {loading && (<Loading/>)}
+            {error && (<Error error='Something went wrong'/>)}
+            {success && (<Success success='Your Order Placed Successfully'/>)}
+
             <StripeCheckout
             amount={subtotal*100}
             shippingAddress
