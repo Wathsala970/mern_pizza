5340c097c62fab736223a27f8c75ef467360f9f1
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 09:28:31 +0530

    create login user action

--- client/src/Components/actions/userAction.js
+++ client/src/Components/actions/userAction.js
@@ -7,9 +7,31 @@ export const registerUser = (user)=> async dispatch=>{
     try {
         const response = await axios.post('/api/users/register', user)
         console.log(response);
-        dispatch({type: 'USER_REGISTER_SUCCES'})
+        dispatch({type: 'USER_REGISTER_SUCCESS'})
     } catch (error) {
-        dispatch({type: 'USER_REGISTER_FAILED' , payload: error})
+        dispatch({type: 'USER_REGISTER_FAILED' , payload: error.message})
         
     }
+}
+
+export const loginUser = (user)=> async dispatch=>{
+    dispatch({type: 'USER_LOGIN_REQUEST'})
+
+    try {
+        const response = await axios.post('/api/users/login', user)
+        console.log(response);
+        dispatch({type: 'USER_LOGIN_SUCCES', payload: response.data})
+        localStorage.setItem('currentUser', JSON.stringify(response.data))
+        // alert('succes')
+        window.location.href='/'
+    } catch (error) {
+        dispatch({type: 'USER_LOGIN_FAILED' , payload: error})
+        // alert('Email or Password Invalid')
+        
+    }
+}
+export const logoutUser = () =>dispatch=>{
+
+    localStorage.removeItem('currentUser')
+    window.location.href('/home')
 }
