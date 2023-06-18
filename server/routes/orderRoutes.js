a42f45feaae64bb850f4f5b4caeb26df78e9da75
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-18 15:45:09 +0530

    create get user order api

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const stripe = require('stripe')('sk_test_51NKDMoKFdiBCuGhvhQd7YwzAZHykQLD5IKEMsuptil0CvzSZJsbeAal19VAyghgaagqqlhPi8S3aOJhbsMAgkz6a00eRG6Cz1H')
+const order = require('../models/orderModel');
+const Router = require('router');
 
 router.post('/placeorder',async (req,res)=>{
 
@@ -24,7 +26,22 @@ router.post('/placeorder',async (req,res)=>{
 
         if(payment)
         {
-            res.send("Payment Done")
+            const neworder = new order({
+                name : currentUser.name,
+                email : currentUser.email,
+                userid : currentUser._id,
+                orderItems : cartItems,
+                orderAmount : subtotal,
+                shippingAddress : {
+                    street : token.card.address_line1,
+                    city : token.card.address_city,
+                    country : token.card.address_country,
+                    pincode : token.card.address_zip
+                },
+                transactionId : payment.source.id
+            })
+            neworder.save()
+            res.send("Order placed successfully")
         }
         else{
             res.send('Payment Failed')
@@ -33,4 +50,15 @@ router.post('/placeorder',async (req,res)=>{
         return res.status(400).json({message: 'Something went wrong' . error})
     }
 });
+
+router.get('/getuserorders',async (req, res)=>{
+    const {userid} = req.body
+    try {
+        const orders = await order.find({userid : userid})
+        res.send(orders)
+    } catch (error) {
+        return res.status(400).json({message : "Something went wrong"})
+    }
+})
+
 module.exports = router
