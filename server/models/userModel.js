e8780ee73d6479058f7d8ed60055ea15553276a9
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-19 22:09:36 +0530

    create user mode

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,6 +4,8 @@ const userSchema = mongoose.Schema({
     name : {type: String , require},
     email : {type: String , require},
     password : {type: String , require},
+    address : {type: String , require},
+    contactno : {type: String , require},
     isAdmin : {type: Boolean, require, default: false},
 },{
     timestaps : true,
