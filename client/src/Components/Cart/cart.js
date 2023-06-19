8ee40bf628217a06f0bd25041c9ffcd532b4dedf
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-19 06:52:34 +0530

    change margin value

--- client/src/Components/Cart/cart.js
+++ client/src/Components/Cart/cart.js
@@ -13,11 +13,11 @@ export default function CartScreen() {
     <div>
       <div className='row '>
         <div className='col m-4 '>
-          <h3>My Cart</h3>
+          <h3>My Cart</h3><hr/>
           {cartItems.map(item => {
 
-            return <div className='flex-container'>
-              <div className='text-left m-4 w-100'>
+            return <div className='flex-container '>
+              <div className=' m-4 w-100'>
                 <h5>{item.name} [{item.varient}]</h5>
                 <h5>Price : {item.quantity} * {item.prices[0][item.varient]}.00 = {item.price}.00</h5>
                 {/* <h5 style={{ display: 'inline' }}>Quantity : </h5>
@@ -31,7 +31,7 @@ export default function CartScreen() {
                 <img src={item.image} style={{ height: '100px', width: '100px' }} />
               </div>
 
-              <div className='m-1 w-50'>
+              <div className='m-1 '>
               <i className="fa fa-trash" aria-hidden="true" onClick={()=>{dispatch(deleteFromCart(item))}}></i>
               </div>
 
@@ -39,9 +39,11 @@ export default function CartScreen() {
           })}
 
         </div>
-        <div className=' col m-5 ' >
+        <div className=' col m-1' >
+          <hr/>
           <h3>Sub Total : {subtotal}.00 /=</h3>
           <Checkout subtotal = {subtotal}/>
+          <hr/>
         </div>
       </div>
     </div>
