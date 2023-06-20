bbee5e2fda016b7c8f1acc185781dc688503b2dd
Wathsala <wathsalaksuriyanpola2000@gmail.com>
2023-06-21 00:47:51 +0530

    server,js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('./server/db')
+const menuRoutes = require('./API/Menu/menu')
+const Pizza = require('./server/models/pizzaModel')
+
+const app = express();
+app.use(express.json());
+
+const pizzasRoute = require('./server/routes/pizzaRaoutes')
+const userRoute = require('./server/routes/userRoutes')
+const orderRoute = require('./server/routes/orderRoutes')
+
+app.use('/api/pizzas/', pizzasRoute)
+app.use('/api/users/', userRoute)
+app.use('/api/orders/', orderRoute)
+
+
+app.get("/", (req,res)=>{
+    res.send("server is working " + port);
+});
+
+
+
+app.use('/menu', menuRoutes)
+const port = process.env.PORT || 8000;
+app.listen(port, ()=> 'server running on port')
