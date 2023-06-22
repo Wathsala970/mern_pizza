a09bf73cb7a35b65f7f6c094c95ccda2635886ac
Wathsala Suriyanpola <100006695+Wathsala970@users.noreply.github.com>
2023-06-22 09:43:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const db = require('./server/db')
 const menuRoutes = require('./API/Menu/menu')
 const Pizza = require('./server/models/pizzaModel')
 const path = require("path")
-
+const cors = require('cors');
 const app = express();
-const cors = require("cors")
 app.use(express.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")))
 
 const pizzasRoute = require('./server/routes/pizzaRaoutes')
@@ -17,6 +15,7 @@ const orderRoute = require('./server/routes/orderRoutes')
 app.use('/api/pizzas/', pizzasRoute)
 app.use('/api/users/', userRoute)
 app.use('/api/orders/', orderRoute)
+app.use(cors());
 
 
 app.get("/", (req,res)=>{
