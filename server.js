63f169726a0162ade128619f6cf6ca1911901db3
Wathsala Suriyanpola <100006695+Wathsala970@users.noreply.github.com>
2023-06-22 10:36:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./server/db')
 const menuRoutes = require('./API/Menu/menu')
 const Pizza = require('./server/models/pizzaModel')
 const path = require("path")
-
+const cors = require("cors")
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")))
@@ -15,10 +15,12 @@ const orderRoute = require('./server/routes/orderRoutes')
 app.use('/api/pizzas/', pizzasRoute)
 app.use('/api/users/', userRoute)
 app.use('/api/orders/', orderRoute)
+app.use(cors());
 
 
 app.get("/", (req,res)=>{
-    res.send("server is working " + port);
+    res.setHeader("Access-Control-Allow-Credentials","true")
+;    res.send("server is working " + port);
 });
 
 
